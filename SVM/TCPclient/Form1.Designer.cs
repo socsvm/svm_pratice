d255b61be0b930d69cc2d092a2ed1f881d9052d1
socsvm <ricky8368@hotmail.com>
2017-06-12 13:59:34 +0800

    Add files via upload

---
+++ SVM/TCPclient/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace TCPclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox6.Location = new System.Drawing.Point(217, 8);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(232, 33);
+            this.textBox6.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(14, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 30);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "TIME:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox5.Location = new System.Drawing.Point(95, 8);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(116, 33);
+            this.textBox5.TabIndex = 20;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(27, 421);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 34);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "label7";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 14.25F);
+            this.button1.Location = new System.Drawing.Point(587, 414);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 46);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "傳送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Cross;
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(33, 48);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(701, 358);
+            this.chart1.TabIndex = 26;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 20F);
+            this.textBox1.Location = new System.Drawing.Point(33, 468);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 43);
+            this.textBox1.TabIndex = 27;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 20F);
+            this.textBox2.Location = new System.Drawing.Point(139, 468);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 43);
+            this.textBox2.TabIndex = 28;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 14.25F);
+            this.button2.Location = new System.Drawing.Point(245, 468);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 46);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "新增訓練資料";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 14.25F);
+            this.button3.Location = new System.Drawing.Point(614, 468);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 46);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "新增辨識點";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("微軟正黑體", 20F);
+            this.textBox3.Location = new System.Drawing.Point(508, 471);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 43);
+            this.textBox3.TabIndex = 31;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("微軟正黑體", 20F);
+            this.textBox4.Location = new System.Drawing.Point(402, 471);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 43);
+            this.textBox4.TabIndex = 30;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.button4.Location = new System.Drawing.Point(700, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 34);
+            this.button4.TabIndex = 34;
+            this.button4.Text = "Start";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(573, 8);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 32);
+            this.comboBox1.TabIndex = 33;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(787, 535);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "SVM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ SVM/TCPclient/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO.Ports;
+
+namespace TCPclient
+{
+    public partial class Form1 : Form
+    {
+
+        byte[] message = new byte[4096];
+
+        byte[] buffer = new byte[26];
+
+        //IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("163.18.59.224"), 4001);
+        //IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.56.1"), 8080);
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("163.18.49.38"), 9999);
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+
+        Thread myThead = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        Stopwatch stopWatch = new Stopwatch();
+            IPEndPoint clientep1;
+            long RX_AA_count = 0;
+            long RX_55_count = 0;
+            int TX_flag = 0;
+            long TX_AA_count = 0;
+            long TX_55_count = 0;
+            int sec = 0;
+            int minute = 0;
+            int hour = 0;    
+        private void BeginListen()
+        {
+            int bytes = 0;
+            
+            while (true)
+            {
+                try
+                {
+                    if (server != null && server.Connected == true)
+                    {
+                        bytes = server.Receive(message);
+                        if (bytes == 5)        //  判斷封包長度
+                        {
+                            Console.WriteLine("bytes：" + bytes);
+                            for (int x = 0; x < bytes; x++)
+                            {
+                                if (message[x] == 0xaa)
+                                {
+                                    RX_AA_count++;
+                                }
+                                if (message[x] == 0x55)
+                                {
+                                    RX_55_count++;
+                                }
+                            }
+                        }
+                    }                               
+                }
+                catch (SocketException se)
+                {           
+
+                }
+            }
+        }
+        #region//声名委托
+        delegate void SetTextCallback(string text, int num);
+       
+        #endregion
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            findSerialPort();       //  搜尋可用com port
+
+            server.Connect(ipep);
+            NetworkStream stream = new NetworkStream(server);
+            StreamReader sr = new StreamReader(stream);
+            StreamWriter sw = new StreamWriter(stream);
+
+            clientep1 = (IPEndPoint)server.RemoteEndPoint;
+            textBox6.Text = "與"+clientep1 + "連線";
+            myThead = new Thread(new ThreadStart(BeginListen));
+            myThead.Start();
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            stopWatch.Start();
+
+            //*****************圖表測試**************
+            //int[,] array = new int[,] {
+            //{1,8,9,7,105,11,50,999,500,1},
+            //{12,15,11,18,733,5,4,3,2,500} };
+
+            int[,] array = new int[,] {
+            {1,8,9,7,15,11,50,99,50,1},
+            {12,15,11,18,73,5,4,3,2,50} };
+
+            int[] svmLine = new int[100];
+            int a, b;   // SVM出來的參數    ax + b = y
+            int maxSize = 0;
+            a = 5; b = 3;
+            for (int i = 0; i < 100; i++)
+            {
+                if (a * i + b < 100)
+                {
+                    svmLine[i] = a * i + b;
+                }
+                else
+                {
+                    maxSize = i-1;
+                    break;
+                }
+            }
+
+            Console.WriteLine("maxSize：" + maxSize);
+            //標題 最大數值
+            Series series1 = new Series("A類", 1000);
+            Series series2 = new Series("B類", 1000);
+            Series series_svmLine = new Series("SVM", a * maxSize + b);
+
+            //設定線條顏色
+            series1.Color = Color.Blue;
+            series2.Color = Color.Red;
+            //series1.Color = Color.White;
+            //series2.Color = Color.White;
+            series_svmLine.Color = Color.Green;
+
+            //設定字型
+            series1.Font = new System.Drawing.Font("微軟正黑體", 12);
+            series2.Font = new System.Drawing.Font("微軟正黑體", 12);
+            series_svmLine.Font = new System.Drawing.Font("微軟正黑體", 12);
+
+            //折線圖
+            series1.ChartType = SeriesChartType.Point;
+            series2.ChartType = SeriesChartType.Point;
+            series_svmLine.ChartType = SeriesChartType.Line;
+
+            //將數值顯示在線上
+            series1.IsValueShownAsLabel = true;
+            series2.IsValueShownAsLabel = true;
+            series_svmLine.IsValueShownAsLabel = false;
+
+            //將數值新增至序列
+            for (int index = 0; index < 10; index++)
+            {
+                series1.Points.AddXY(index, array[0, index]);
+                series2.Points.AddXY(index, array[1, index]);
+            }
+
+            //series1.Points.AddXY( 80);
+
+            //  繪製SVM分割線
+            for (int i = 0; i <= maxSize; i++)
+            {
+                series_svmLine.Points.AddXY(i, svmLine[i]);
+            }
+
+            //將序列新增到圖上
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series_svmLine);
+            
+            //*******************************************
+
+
+
+
+        }
+
+        private void findSerialPort()       //  搜尋可用com port
+        {
+            string[] ports = SerialPort.GetPortNames();// Get a list of serial port names.
+
+            if (ports.Length == 0)
+            {
+                MessageBox.Show("找不到COM port", "");
+                Environment.Exit(1); //結束程序
+            }
+
+            foreach (string s in ports)
+                comboBox1.Items.Add(s);
+            comboBox1.SelectedIndex = 0; //預設第一個子選項索引值
+
+            //throw new NotImplementedException();
+        }
+
+        private void timer1_Tick_1(object sender, System.EventArgs e)
+        {
+            byte[] TX_aa = new byte[] { 0xaa };
+            byte[] TX_55 = new byte[] { 0x55 };
+
+
+            //if (server != null && server.Connected == true)
+            //{
+            //    if (TX_flag == 1)
+            //    {
+            //        TX_flag = 0;
+            //        server.Send(TX_aa, 1, 0);
+            //        TX_AA_count++;
+            //    }
+            //    else
+            //    {
+            //        TX_flag = 1;
+            //        server.Send(TX_55, 1, 0);
+            //        TX_55_count++;
+            //    }
+            //}
+
+
+            
+        }
+
+        private void timer2_Tick_1(object sender, System.EventArgs e)
+        {
+            if (stopWatch.ElapsedMilliseconds >= 1000)
+            {
+                sec++;
+                if (sec > 59)
+                {
+                    sec = 0;
+                    minute++;
+                    if (minute > 59)
+                    {
+                        minute = 0;
+                        hour++;
+                    }
+                }
+                textBox5.Text = hour.ToString("D2") + ":" + minute.ToString("D2") + ":" + sec.ToString("D2");
+
+                stopWatch.Restart();
+            }
+            
+
+            //for (int i = 0; i < message.Length; i++)
+            //{
+            //    label7.Text = "" + message[i].ToString();
+            //}
+            //label7.Text = "" + message[0].ToString();
+            label7.Text = System.Text.Encoding.UTF8.GetString(message);
+        }
+
+        private void button1_Click(object sender, EventArgs e)      //  send測試資料
+        {
+            String stringData = "ABC\n";
+            //byte[] Data = new byte[] { 0xaa };
+            byte[] Data = System.Text.Encoding.Default.GetBytes(stringData);
+            server.Send(Data);
+        }
+
+        private void button4_Click(object sender, EventArgs e)      //  com port連線按鈕
+        {
+            try
+            {
+                serialPort1.Close();
+                this.serialPort1 = new SerialPort(comboBox1.Text, 9600, Parity.None, 8, StopBits.One);
+                serialPort1.Open();
+                timer1.Enabled = true;
+            }
+            catch
+            {
+            }
+        }
+    }
+
+   
+
+
+}
